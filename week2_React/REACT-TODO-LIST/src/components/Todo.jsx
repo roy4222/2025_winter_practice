529d12b6ef0f6632cb55c3d48465f77ee8e395a4
roy4222 <roy422roy@gmail.com>
2025-01-21 21:30:02 +0800

    刪除功能跟勾選功能實裝

--- week2_React/REACT-TODO-LIST/src/components/Todo.jsx
+++ week2_React/REACT-TODO-LIST/src/components/Todo.jsx
@@ -1,7 +1,35 @@
-function Todo({ todo }) {
-    return <div className="todo">
-        <p>{todo.content}</p>
-    </div>
+// 引入 React 圖標庫中的刪除和編輯圖標
+import { MdDelete, MdEdit } from "react-icons/md";
+
+// 定義 Todo 組件，接受 todo 對象和相關操作函數作為 props
+function Todo({ todo , deleteTodo, editTodo , toggleCompleted}) {
+    return (
+        // 創建待辦事項容器，根據完成狀態添加 completed 類
+        <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+            {/* 顯示待辦事項內容，點擊時切換完成狀態 */}
+            <input 
+                type="checkbox" 
+                checked={todo.isCompleted}
+                onChange={() => toggleCompleted(todo.id)}
+                style={{ marginRight: "10px" }}
+            />
+
+            <p>{todo.content}</p>
+            <div>
+                {/* 編輯按鈕，點擊時調用 editTodo 函數 */}
+                <MdEdit 
+                    style={{cursor: "pointer"}} 
+                    onClick={() => editTodo(todo.id)}
+                />
+                {/* 刪除按鈕，點擊時調用 deleteTodo 函數 */}
+                <MdDelete 
+                    style={{cursor: "pointer" , marginLeft: "5px"}} 
+                    onClick={() => deleteTodo(todo.id)}
+                />
+            </div>
+        </div>
+    );
 }
 
-export default Todo 
+// 導出 Todo 組件
+export default Todo;
--- week2_React/REACT-TODO-LIST/src/components/TodoWrapper.jsx
+++ week2_React/REACT-TODO-LIST/src/components/TodoWrapper.jsx
@@ -7,10 +7,10 @@ function TodoWrapper() {
     // 使用 useState hook 來管理待辦事項列表
     // todos 是當前的待辦事項列表，setTodos 是用來更新列表的函數
     const [todos, setTodos] = useState([
-        {content:'吃飯', id:Math.random()},     // 初始化一些待辦事項
-        {content:'睡覺', id:Math.random()},     // 每個待辦事項都有內容和唯一的 id
-        {content:'寫作業', id:Math.random()},   // id 使用 Math.random() 生成，確保唯一性
-        {content:'打掃廁所', id:Math.random()}, // 這種方式在實際應用中可能需要更可靠的 id 生成方法
+        {content:'吃飯', id:Math.random(),isCompleted:false},     // 初始化一些待辦事項
+        {content:'睡覺', id:Math.random(),isCompleted:false},     // 每個待辦事項都有內容和唯一的 id
+        {content:'寫作業', id:Math.random(),isCompleted:false},   // id 使用 Math.random() 生成，確保唯一性
+        {content:'打掃廁所', id:Math.random(),isCompleted:false}, // 這種方式在實際應用中可能需要更可靠的 id 生成方法
     ]);
 
     // 定義 addTodo 函數，接受一個 todo 參數（新的待辦事項內容）
@@ -27,12 +27,31 @@ function TodoWrapper() {
         ]);
     }
 
+    // 刪除指定 id 的待辦事項
+    const deleteTodo = (id) => {
+        // 使用 filter 方法創建新的陣列，排除指定 id 的待辦事項
+        setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
+    // 切換指定 id 待辦事項的完成狀態
+    const toggleCompleted = (id) => {
+        // 使用 map 方法創建新的陣列，修改指定 id 待辦事項的狀態
+        setTodos(todos.map((todo) => {
+            if (todo.id === id) {
+                // 如果 id 匹配，創建新對象並反轉 isCompleted 狀態
+                return {...todo, isCompleted: !todo.isCompleted}
+            }
+            // 如果 id 不匹配，保持原樣
+            return todo
+        }))
+    }
+
     return (
         <div className="wrapper">
             <h1>代辦事項</h1>
             <CreateForm addTodo={addTodo} />
             {todos.map((todo, index) => (
-                <Todo key={todo.id} todo={todo} />
+                <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleCompleted={toggleCompleted}/>
             ))}
         </div>
     )
