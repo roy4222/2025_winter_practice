8336561db2dffa3574dc353f2bfc68dcbd529306
roy4222 <roy422roy@gmail.com>
2025-01-21 16:58:33 +0800

    todo列表表頭實作

---
+++ week2_React/REACT-TODO-LIST/src/components/TodoWrapper.jsx
@@ -0,0 +1,12 @@
+import CreateForm from "./CreateForm"
+
+function TodoWrapper() {
+    return (
+        <div className="wrapper">
+            <h1>代辦事項</h1>
+            <CreateForm />
+        </div>
+    )
+}
+
+export default TodoWrapper
