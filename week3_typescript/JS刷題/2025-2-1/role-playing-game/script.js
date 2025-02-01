11b4dd4d1a9ac4446c70ea624108677a8412ad9e
roy4222 <roy422roy@gmail.com>
2025-02-01 23:35:30 +0800

    js基礎語法day2-last

--- week3_typescript/JS刷題/2025-2-1/role-playing-game/script.js
+++ week3_typescript/JS刷題/2025-2-1/role-playing-game/script.js
@@ -1,4 +1,3 @@
-// 初始化玩家狀態
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -7,7 +6,6 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-// 獲取DOM元素
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -18,56 +16,70 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const locations = ["town", "store", "cave"];
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
+  }
+];
 
-// 初始化按鈕點擊事件
+// initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// 更新遊戲狀態
 function update(location) {
-
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
-// 前往城鎮
 function goTown() {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  update(locations[0]);
 }
 
-// 前往商店
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+  update(locations[1]);
 }
 
-// 前往洞穴
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
-// 與龍戰鬥
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-// 購買生命值
 function buyHealth() {
+  gold = gold - 10;
+}
+
+function buyWeapon() {
 
 }
 
-// 購買武器
-function buyWeapon() {
+function fightSlime() {
+
+}
+
+function fightBeast() {
 
 }
